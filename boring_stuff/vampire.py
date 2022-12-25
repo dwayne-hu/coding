5291231d9547eb41d7773bda5a80850d76c2a60a
Dwayne Hullermann <dwayne.hullermann@posteo.de>
2022-12-25 11:23:21 +0100

    initial commit

---
+++ boring_stuff/vampire.py
@@ -0,0 +1,10 @@
+name = 'Carol'
+age = 3000
+if name == 'Alice':
+    print('Hi, Alice!')
+elif age < 12:
+    print('You are not Alice,  Kiddo')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead vampire.')
+elif age > 100:
+    print('Granny!')
---
+++ boring_stuff/yourName.py
@@ -0,0 +1,5 @@
+name = ''
+while name != 'dwayne':
+    print('Please type your name!')
+    name = input()
+print('Access granted!')
